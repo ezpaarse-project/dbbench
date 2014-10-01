ede2a376b087dfec3c867f9744066f5bce2bd596
Yannick Schurter <yannick.schurter@inist.fr>
2014-10-01 16:15:18 +0200

    add memory db

---
+++ plug/memory/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+var db = {};
+
+exports.name = 'Memory';
+
+exports.init = function (callback) {
+  callback();
+};
+
+exports.insert = function (id, entry, callback) {
+  db[id] = entry;
+  callback(null);
+};
+
+exports.get = function (id, callback) {
+  var entry = db[id];
+
+  if (entry) {
+    callback(null, entry);
+  } else {
+    console.log(id)
+    callback(new Error('ID not found'));
+  }
+};
