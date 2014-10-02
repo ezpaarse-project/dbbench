e3daa035a1c4621345845d32a6554e28b3676874
Yannick Schurter <yannick.schurter@inist.fr>
2014-10-02 17:36:43 +0200

    add PouchDB

---
+++ plug/pouchdb/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var PouchDB = require('pouchdb');
+var db;
+
+exports.name = 'PouchDB';
+
+exports.init = function (options, callback) {
+  PouchDB.destroy('items', function (err) {
+    if (err) { return callback(err); }
+
+    db = new PouchDB('items');
+    callback();
+  });
+};
+
+exports.insert = function (id, entry, callback) {
+  db.put(entry, id, callback);
+};
+
+exports.get = function (id, callback) {
+  db.get(id, callback);
+};
